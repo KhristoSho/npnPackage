80d36fe34b21ba212a4ccdc3afea8c0023dfe107
KhristSho <ShonusKhristo@yandex.ru>
2026-02-08 16:13:38 +0700

    fix eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,10 @@ module.exports = {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
   },
-  'rules': {}
-}
+  'rules': {
+    'semi': ['error', 'always'], // Требует точку с запятой в конце выражений
+    'quotes': ['error', 'single'], // Требует одинарные кавычки для строк
+    'no-console': 'warn', // Предупреждает об использовании console
+    'no-unused-vars': 'error' // Ошибка при неиспользуемых переменных
+  }
+};
